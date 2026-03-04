3524ee60080a96344a5712212e9d3e5b845827cd
Amruth Kumar TJ <amruthkumartj@gmail.com>
2026-03-04 20:08:12 +0530

    Updates

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
-//const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
 // parse requests of content-type - application/json
 app.use(express.json());
-
+app.use(cors());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 require("./app/routes/turorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
